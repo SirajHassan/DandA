9030cb56b7a3c899a08068d30c4382c9bdca4742
Siraj Hassan <sirajhassan@yahoo.com>
2020-01-29 21:30:15 -0600

    Updated Recrusive Version

--- LinkedLists/DoublyNode.py
+++ LinkedLists/DoublyNode.py
@@ -14,7 +14,7 @@ class DoublyNode(Node):
         else:
             print("(" + str(self.data) + ")")
 
-    # 
+    #Append node to end
     def append_to_tail(self,new_node):
         if (self.next == None):
             self.next = new_node
@@ -30,6 +30,7 @@ class DoublyNode(Node):
     def append_to_tail_recursive(self,new_node):
         if self.next == None:
             self.next = new_node
+            self.next.previous = self
             return
         else:
             self.next.append_to_tail_recursive(new_node)
